e96078dde1c3f3e0ec38a5880712240567118163
timothywvance <tvance@dckids.org>
2016-02-18 08:56:41 -0500

    fetch ward

--- data/scripts/fetch_acs.py
+++ data/scripts/fetch_acs.py
@@ -2,16 +2,16 @@
 import itertools, json, math, os, re, sys, urllib2, yaml
 
 DEFAULT_OUTPUT_DIRECTORY = "outputs"
-DEFAULT_OUTPUT_FILE = "acs_SLDU_data.json"
+DEFAULT_OUTPUT_FILE = "acs_tract_data.json"
 DEFAULT_OUTPUT_ACS_VARIABLES = False
 
 def census_query(acs_variables, base_url, geo):
     """This function performs the Census API query. It returns hashes of the form
-    {SLDU1 => {var1 => val1, var2 => val2, ...},
-    {SLDU2 => {var1 => val1, var2 => val2, ...}, ... }"""
+    {tract1 => {var1 => val1, var2 => val2, ...},
+    {tract2 => {var1 => val1, var2 => val2, ...}, ... }"""
 
     acs_variables = list(acs_variables)
-    SLDU = {}
+    tracts = {}
     start_index = 0
 
     while start_index <= len(acs_variables):
@@ -33,16 +33,16 @@ def census_query(acs_variables, base_url, geo):
                 if r[k] is not None and k not in ["state", "state legislative district (upper chamber)"]:
                     r[k] = int(r[k])
 
-            # SLDU ids in the neighborhood file include the state and county number
-            SLDU_id = "11" + r["state legislative district (upper chamber)"]
-            if SLDU_id in SLDU:
-                SLDU[SLDU_id].update(r)
+            # tract ids in the neighborhood file include the state and county number
+            tract_id = r["state legislative district (upper chamber)"]
+            if tract_id in tracts:
+                tracts[tract_id].update(r)
             else:
-                SLDU[SLDU_id] = r
+                tracts[tract_id] = r
 
         start_index += 50
 
-    return SLDU
+    return tracts
 
 
 class ConfigurationException(Exception): pass
@@ -116,34 +116,34 @@ def main(args):
     all_acs_fields |= set(itertools.chain(*fields_prod.values()))
 
     all_acs_fields |= set(re.sub("E", "M", s) for s in all_acs_fields)
-    SLDU = census_query(all_acs_fields, base_url, geo)
+    tracts = census_query(all_acs_fields, base_url, geo)
 
     # print all_acs_fields
-    # print SLDU['11001000300']
+    # print tracts['11001000300']
 
     # compute named variables from ACS variables
 
-    for SLDU_id, SLDU in SLDU.items():
+    for tract_id, tract in tracts.items():
 
         for outname, acsname in fields_rename.items():
             errname = re.sub("E", "M", acsname)
-            SLDU[outname] = SLDU[acsname]
-            SLDU[outname + "_margin"] = SLDU[errname]
+            tract[outname] = tract[acsname]
+            tract[outname + "_margin"] = tract[errname]
 
         # # We found some Census documentation suggesting that we use the
         # # sqrt(sum of squares) for the margin of error for aggregate estimates.
         # # https://www.census.gov/acs/www/Downloads/data_documentation/Statistical_Testing/ACS_2008_Statistical_Testing.pdf
 
         for outname, acsfields in fields_sum.items():
-            SLDU[outname] = 0
-            SLDU[outname + "_margin"] = 0
+            tract[outname] = 0
+            tract[outname + "_margin"] = 0
 
             for acsname in acsfields:
                 errname = re.sub("E", "M", acsname)
-                SLDU[outname] += SLDU[acsname]
-                SLDU[outname + "_margin"] += (SLDU[errname] / 1.645) ** 2
+                tract[outname] += tract[acsname]
+                tract[outname + "_margin"] += (tract[errname] / 1.645) ** 2
 
-            SLDU[outname + "_margin"] = math.sqrt(SLDU[outname + "_margin"]) * 1.645
+            tract[outname + "_margin"] = math.sqrt(tract[outname + "_margin"]) * 1.645
 
         for outname, acsfields in fields_sub.items():
             # first, *rest = acsfields
@@ -151,37 +151,37 @@ def main(args):
             rest = acsfields[1:]
 
             errname = re.sub("E", "M", first)
-            SLDU[outname] = SLDU[first]
-            SLDU[outname + "_margin"] = (SLDU[errname] / 1.645) ** 2
+            tract[outname] = tract[first]
+            tract[outname + "_margin"] = (tract[errname] / 1.645) ** 2
 
             for acsname in rest:
                 errname = re.sub("E", "M", acsname)
-                SLDU[outname] -= SLDU[acsname]
-                SLDU[outname + "_margin"] += (SLDU[errname] / 1.645) ** 2
+                tract[outname] -= tract[acsname]
+                tract[outname + "_margin"] += (tract[errname] / 1.645) ** 2
 
-            SLDU[outname + "_margin"] = math.sqrt(SLDU[outname + "_margin"]) * 1.645
+            tract[outname + "_margin"] = math.sqrt(tract[outname + "_margin"]) * 1.645
 
         # TODO: compute margin of error for product field
 
         for outname, acsfields in fields_prod.items():
-            SLDU[outname] = 1
-            SLDU[outname + "_margin"] = 0
+            tract[outname] = 1
+            tract[outname + "_margin"] = 0
 
             for acsname in acsfields:
                 try:
-                    SLDU[outname] *= SLDU[acsname]
+                    tract[outname] *= tract[acsname]
                 except TypeError:
                     pass
 
     # remove ACS variables from output
     if not output_acs_variables:
-        for SLDU in SLDU.values():
+        for tract in tracts.values():
             for f in all_acs_fields:
-                del(SLDU[f])
+                del(tract[f])
 
     # write the result to a JSON file
     with open(output_file, "w") as f:
-        json.dump(SLDU, f, indent=2)
+        json.dump(tracts, f, indent=2)
 
 
 if __name__ == "__main__":
