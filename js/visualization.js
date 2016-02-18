804b55555dca986dbcbfda6da0bc4fbd7f1f317f
timothywvance <tvance@dckids.org>
2016-02-18 09:45:11 -0500

    zoom 4

--- js/visualization.js
+++ js/visualization.js
@@ -169,9 +169,9 @@ function drawChoropleth(){
     displayPopBox();
 
     gmap = new google.maps.Map(d3.select("#content").node(), {
-      zoom: 7,
-      minZoom: 7,
-      maxZoom: 7,
+      zoom: 5,
+      minZoom: 5,
+      maxZoom: 5,
       center: new google.maps.LatLng(38.910,-75.527),
       mapTypeId: google.maps.MapTypeId.ROADMAP,
       streetViewControl: false,
