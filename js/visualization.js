f3551081ac2af563763bdc6e475c8abc9da64f8a
timothywvance <tvance@dckids.org>
2016-06-22 12:55:00 -0400

    Maine 5

--- js/visualization.js
+++ js/visualization.js
@@ -169,9 +169,9 @@ function drawChoropleth(){
     displayPopBox();
 
     gmap = new google.maps.Map(d3.select("#content").node(), {
-      zoom: 7,
-      minZoom: 6,
-      maxZoom: 9,
+      zoom: 5,
+      minZoom: 4,
+      maxZoom: 7,
       center: new google.maps.LatLng(45.2538,-69.4455),
       mapTypeId: google.maps.MapTypeId.ROADMAP,
       streetViewControl: false,
